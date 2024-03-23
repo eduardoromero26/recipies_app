cf91cd81a7642304e809698a769a1dc586226bc0
eduardodejesusromero <eduardodejesus.romero@nttdata.com>
2024-03-22 20:51:54 -0600

    RA_017:Create style folder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipies_app/style/font_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,7 +11,10 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Recipies App'),
       ),
       body: const Center(
-        child: Text('Home Screen'),
+        child: Text(
+          'Home Screen',
+          style: TypographyTheme.fontSemi24Px,
+        ),
       ),
     );
   }
---
+++ lib/style/font_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:recipies_app/style/color_theme.dart';
+
+class TypographyTheme {
+  static const fontSemi24Px = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Rubik',
+      color: ColorsTheme.primaryColor);
+  static const fontSemi20Px = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Rubik',
+      color: ColorsTheme.headingColor);
+  static const fontMedium20Px = TextStyle(
+      fontSize: 20, fontFamily: 'Rubik', color: ColorsTheme.headingColor);
+  static const fontRegular16Px = TextStyle(
+      fontSize: 16, fontFamily: 'Rubik', color: ColorsTheme.textColor);
+  static const fontLight14Px = TextStyle(
+      fontSize: 14, fontFamily: 'Rubik', color: ColorsTheme.textColor);
+}
