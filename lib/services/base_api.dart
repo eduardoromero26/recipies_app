2f79f091b9ab0a7fa13e085d5001336ff8843fba
eduardodejesusromero <eduardodejesus.romero@nttdata.com>
2024-03-23 10:19:28 -0600

    RA_005:Use dio to handle http request

---
+++ lib/services/base_api.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class BaseApi {
+  final dio = Dio();
+
+  Future getFromApi(String endpoint) async {
+    try {
+      final response = await dio.get('${dotenv.env['API_URL']}$endpoint');
+      return response.data;
+    } on DioException catch (e) {
+      return e.message;
+    }
+  }
+}
