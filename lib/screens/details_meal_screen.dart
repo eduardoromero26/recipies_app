9d1e69fe38088793248cf10fc24427a4a83993c2
eduardodejesusromero <eduardodejesus.romero@nttdata.com>
2024-03-25 21:12:51 -0600

    Refactor details_meal_screen.dart

--- lib/screens/details_meal_screen.dart
+++ lib/screens/details_meal_screen.dart
@@ -1,8 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lottie/lottie.dart';
-import 'package:recipies_app/bloc/recipies_bloc.dart';
 import 'package:recipies_app/models/meal_model.dart';
 import 'package:recipies_app/style/color_theme.dart';
 import 'package:recipies_app/style/font_styles.dart';
@@ -23,49 +20,53 @@ class _DetailsMealScreenState extends State<DetailsMealScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          backgroundColor: ColorsTheme.primaryColor,
-          leading: IconButton(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all(Colors.white),
-              backgroundColor:
-                  MaterialStateProperty.all(ColorsTheme.primaryColor),
+    return SafeArea(
+      child: Scaffold(
+          body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: ColorsTheme.primaryColor,
+            leading: IconButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                backgroundColor:
+                    MaterialStateProperty.all(ColorsTheme.primaryColor),
+              ),
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          expandedHeight: MediaQuery.of(context).size.height * 0.4,
-          floating: false,
-          flexibleSpace: FlexibleSpaceBar(
-            background: Hero(
-              tag: widget.selectedMeal.idMeal,
-              child: CachedNetworkImage(
-                imageUrl: widget.selectedMeal.strMealThumb,
-                fit: BoxFit.cover,
-                placeholder: (context, url) => const SizedBox(
-                    height: 40, width: 40, child: CircularProgressIndicator()),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
+            expandedHeight: MediaQuery.of(context).size.height * 0.4,
+            floating: false,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Hero(
+                tag: widget.selectedMeal.idMeal,
+                child: CachedNetworkImage(
+                  imageUrl: widget.selectedMeal.strMealThumb,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => const SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
               ),
             ),
           ),
-        ),
-        SliverToBoxAdapter(
-          child: widget.selectedMeal.strMeal.isNotEmpty
-              ? Column(
-                  children: [
-                    Text(widget.selectedMeal.strMeal,
-                        style: TypographyTheme.fontSemi20Px),
-                    const SizedBox(height: 1000),
-                  ],
-                )
-              : const Text('Meal not found'),
-        ),
-      ],
-    ));
+          SliverToBoxAdapter(
+            child: widget.selectedMeal.strMeal.isNotEmpty
+                ? Column(
+                    children: [
+                      Text(widget.selectedMeal.strMeal,
+                          style: TypographyTheme.fontSemi20Px),
+                      const SizedBox(height: 1000),
+                    ],
+                  )
+                : const Text('Meal not found'),
+          ),
+        ],
+      )),
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,161 +24,166 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: CustomScrollView(
-      slivers: <Widget>[
-        const SliverToBoxAdapter(
-            child: SizedBox(
-          height: 20,
-        )),
-        SliverPinnedHeader(
-            child: Container(
-          color: Colors.white,
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            controller: context.read<RecipiesBloc>().searchFieldController,
-            decoration: InputDecoration(
-                border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                hintText: 'Search for meals',
-                hintStyle: TypographyTheme.fontMedium20Px,
-                prefixIcon: const Icon(Icons.search),
-                suffixIcon: IconButton(
-                    icon: const Icon(Icons.clear),
-                    onPressed: () {
-                      context
+    return SafeArea(
+      child: Scaffold(
+          body: CustomScrollView(
+        slivers: <Widget>[
+          SliverPinnedHeader(
+              child: Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: context.read<RecipiesBloc>().searchFieldController,
+              decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  hintText: 'Search for meals',
+                  hintStyle: TypographyTheme.fontMedium20Px,
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: context
                           .read<RecipiesBloc>()
-                          .add(ResetSearchControllerEvent());
-                      context
-                          .read<RecipiesBloc>()
-                          .add(SearchMealByNameEvent(name: ''));
-                    })),
-            onSubmitted: (value) {
-              context
-                  .read<RecipiesBloc>()
-                  .add(SearchMealByNameEvent(name: value));
-            },
-          ),
-        )),
-        BlocConsumer<RecipiesBloc, RecipiesState>(
-          listener: (BuildContext context, RecipiesState state) {},
-          builder: (context, state) {
-            return state.when(initial: () {
-              return const SliverFillRemaining(
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }, loadingStarted: () {
-              return SliverFillRemaining(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Lottie.asset('assets/lotties/progress_lottie.json',
-                      width: MediaQuery.of(context).size.width * 0.5),
-                  const Text(
-                    'Loading meals...',
-                    style: TypographyTheme.fontSemi20Px,
+                          .searchFieldController
+                          .text
+                          .isNotEmpty
+                      ? IconButton(
+                          icon: const Icon(Icons.clear),
+                          onPressed: () {
+                            context
+                                .read<RecipiesBloc>()
+                                .add(ResetSearchControllerEvent());
+                            context
+                                .read<RecipiesBloc>()
+                                .add(SearchMealByNameEvent(name: ''));
+                          })
+                      : const SizedBox.shrink()),
+              onSubmitted: (value) {
+                context
+                    .read<RecipiesBloc>()
+                    .add(SearchMealByNameEvent(name: value));
+              },
+            ),
+          )),
+          BlocConsumer<RecipiesBloc, RecipiesState>(
+            listener: (BuildContext context, RecipiesState state) {},
+            builder: (context, state) {
+              return state.when(initial: () {
+                return const SliverFillRemaining(
+                  child: Center(
+                    child: CircularProgressIndicator(),
                   ),
-                ],
-              ));
-            }, loadedSuccess: (MealsModel? meals) {
-              return context.read<RecipiesBloc>().mealList!.meals!.isNotEmpty
-                  ? BlocBuilder<RecipiesBloc, RecipiesState>(
-                      builder: (context, state) {
-                      return SliverList.builder(
-                        addAutomaticKeepAlives: true,
-                        itemCount: context
-                            .read<RecipiesBloc>()
-                            .mealList!
-                            .meals!
-                            .length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListTile(
-                            title: Text(
-                                context
-                                    .read<RecipiesBloc>()
-                                    .mealList!
-                                    .meals![index]
-                                    .strMeal,
-                                style: TypographyTheme.fontSemi20Px),
-                            subtitle: Text(
-                                context
-                                    .read<RecipiesBloc>()
-                                    .mealList!
-                                    .meals![index]
-                                    .strCategory,
-                                style: TypographyTheme.fontRegular16Px),
-                            leading: Hero(
-                              tag: context
-                                  .read<RecipiesBloc>()
-                                  .mealList!
-                                  .meals![index]
-                                  .idMeal,
-                              child: CachedNetworkImage(
-                                imageUrl: context
-                                    .read<RecipiesBloc>()
-                                    .mealList!
-                                    .meals![index]
-                                    .strMealThumb,
-                                placeholder: (context, url) =>
-                                    const CircularProgressIndicator(),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                              ),
-                            ),
-                            trailing: const Icon(Icons.arrow_forward_ios),
-                            onTap: () {
-                              Navigator.pushNamed(context, RouteScreens.details,
-                                  arguments: context
+                );
+              }, loadingStarted: () {
+                return SliverFillRemaining(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Lottie.asset('assets/lotties/progress_lottie.json',
+                        width: MediaQuery.of(context).size.width * 0.5),
+                    const Text(
+                      'Loading meals...',
+                      style: TypographyTheme.fontSemi20Px,
+                    ),
+                  ],
+                ));
+              }, loadedSuccess: (MealsModel? meals) {
+                return context.read<RecipiesBloc>().mealList!.meals!.isNotEmpty
+                    ? BlocBuilder<RecipiesBloc, RecipiesState>(
+                        builder: (context, state) {
+                        return SliverList.builder(
+                          addAutomaticKeepAlives: true,
+                          itemCount: context
+                              .read<RecipiesBloc>()
+                              .mealList!
+                              .meals!
+                              .length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              title: Text(
+                                  context
                                       .read<RecipiesBloc>()
                                       .mealList!
-                                      .meals![index]);
-                            },
-                          );
-                        },
-                      );
-                    })
-                  : SliverFillRemaining(
-                      child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Lottie.asset('assets/lotties/not_found_lottie.json',
-                            width: MediaQuery.of(context).size.width * 0.5),
-                        Text(
-                          'Not meals founded: ${context.read<RecipiesBloc>().searchFieldController.text}',
-                          style: TypographyTheme.fontSemi20Px,
-                        ),
-                      ],
-                    ));
-            }, loadedFailed: (String message) {
-              return SliverFillRemaining(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Lottie.asset('assets/lotties/error_lottie.json',
-                      width: 200, height: 200),
-                  const SizedBox(height: 40),
-                  const Text(
-                    'Error during the network request',
-                    style: TypographyTheme.fontSemi20Px,
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      context
-                          .read<RecipiesBloc>()
-                          .add(SearchMealByNameEvent(name: ''));
-                    },
-                    child: const Text('Try again',
-                        style: TypographyTheme.fontSemi20Px),
-                  ),
-                ],
-              ));
-            });
-          },
-        ),
-      ],
-    ));
+                                      .meals![index]
+                                      .strMeal,
+                                  style: TypographyTheme.fontSemi20Px),
+                              subtitle: Text(
+                                  context
+                                      .read<RecipiesBloc>()
+                                      .mealList!
+                                      .meals![index]
+                                      .strCategory,
+                                  style: TypographyTheme.fontRegular16Px),
+                              leading: Hero(
+                                tag: context
+                                    .read<RecipiesBloc>()
+                                    .mealList!
+                                    .meals![index]
+                                    .idMeal,
+                                child: CachedNetworkImage(
+                                  imageUrl: context
+                                      .read<RecipiesBloc>()
+                                      .mealList!
+                                      .meals![index]
+                                      .strMealThumb,
+                                  placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                ),
+                              ),
+                              trailing: const Icon(Icons.arrow_forward_ios),
+                              onTap: () {
+                                Navigator.pushNamed(
+                                    context, RouteScreens.details,
+                                    arguments: context
+                                        .read<RecipiesBloc>()
+                                        .mealList!
+                                        .meals![index]);
+                              },
+                            );
+                          },
+                        );
+                      })
+                    : SliverFillRemaining(
+                        child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Lottie.asset('assets/lotties/not_found_lottie.json',
+                              width: MediaQuery.of(context).size.width * 0.5),
+                          Text(
+                            'Not meals founded: ${context.read<RecipiesBloc>().searchFieldController.text}',
+                            style: TypographyTheme.fontSemi20Px,
+                          ),
+                        ],
+                      ));
+              }, loadedFailed: (String message) {
+                return SliverFillRemaining(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Lottie.asset('assets/lotties/error_lottie.json',
+                        width: 200, height: 200),
+                    const SizedBox(height: 40),
+                    const Text(
+                      'Error during the network request',
+                      style: TypographyTheme.fontSemi20Px,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        context
+                            .read<RecipiesBloc>()
+                            .add(SearchMealByNameEvent(name: ''));
+                      },
+                      child: const Text('Try again',
+                          style: TypographyTheme.fontSemi20Px),
+                    ),
+                  ],
+                ));
+              });
+            },
+          ),
+        ],
+      )),
+    );
   }
 }
